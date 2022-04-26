472dbb02984a703f5d736ef4d04036146fd04540
Will Atlas <atlas@falcon.local>
2022-04-25 21:15:51 -0600

    Fix check logic for healthchecks.io

--- plugins/healthchecks_io/start.sh
+++ plugins/healthchecks_io/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ -n "$HEALTHCHECKS_IO_CHECK_KEY" ]]; then
-  echo "Healthchecks.io is disabled, exiting..."
-  exit 0
+  while true
+   do
+     curl -m 10 --retry 5 https://hc-ping.com/${HEALTHCHECKS_IO_CHECK_KEY}
+     sleep 60
+   done
 fi
 
-while true
- do
-   curl -m 10 --retry 5 https://hc-ping.com/${HEALTHCHECKS_IO_CHECK_KEY}
-   sleep 60
- done
+echo "Healthchecks.io is disabled, exiting..."
+exit 0
